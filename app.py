cecb967df6b8bc8d387d57677f1e3d0fc1319da1
snehith57624 <snehithb57624@gmail.com>
2023-09-18 15:24:18 +0000

    Adding swagger

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ from flask import Flask, jsonify, request
 # Server startup
 from gevent.pywsgi import WSGIServer
 import subprocess
+from gevent.pywsgi import WSGIServer
+from flask_restx import Api, Resource
 
 dir_name = ""
 app = Flask(__name__)
+api = Api(app, version='1.0', title='PA1 Score', description='Get Score of PA1 assignment', doc='/')
 
 def get_score(file_path):
     # Define the file path
@@ -30,54 +33,51 @@ def get_score(file_path):
     return last_score
 
 
-# Flask App Creation
-def create_app():
-    # app = Flask(__name__)
-
-    @app.route('/score', methods=['POST'])
-    def train_model():
+@api.route('/score')
+class Score(Resource):
+    @api.doc(params={'github_repository_url': 'GitHub Repository URL'})
+    def post(self):
         global dir_name
-        data = request.json
-        print(data)
-        github_repository_url = data.get('github_repository_url')
+        # print(request.args)
+        args = request.args
+        # print(args.get("github_repository_url"))
+        
+        github_repository_url = args.get("github_repository_url")
         dir_name = github_repository_url.split("/")[-1][:-4]
         os.system("rm -rf " + dir_name)
         try:
-            print(os.getcwd())
-            # curr_dir = "/Users/snehithbikumandla/PycharmProjects/Pa1Test/"
-
-            # os.system("cd " + curr_dir + "/temp")
+            # print(os.getcwd())
             os.system("git clone " + github_repository_url)
             curr_dir = os.getcwd()+"/"+dir_name+"/output.txt"
-            os.system("cd " + dir_name + " && rm -rf pa1-tests.sh && rm -rf output.txt && mkdir received && cp ../../pa1-tests.sh . && bash pa1-tests.sh >> output.txt")
+            os.system("cd " + dir_name + " && rm -rf pa1-tests.sh && rm -rf output.txt && mkdir received && cp ../pa1-tests.sh . && bash pa1-tests.sh >> output.txt")
             val = get_score(curr_dir)
             print(val)
             print(curr_dir)
-            resp = jsonify({'message': val})
+            resp = jsonify({'Score': val})
             resp.status_code = 200
 
         except Exception as e:
             resp = jsonify(({"result": e}))
             resp.status_code = 400
-        # finally:
-        #     os.system("rm -rf " + dir_name)
+        finally:
+            os.system("rm -rf " + dir_name)
 
         return resp
 
-    # Method to test if the docker is operating
-    @app.route('/test', methods=['GET'])
-    def test_model():
+# Method to test if the docker is operating
+@api.route('/test')
+class Test(Resource):
+    def get(self):
         # Only return that it is working
         # Useful to test the network connectivity
         resp = jsonify({"result": "Working"})
         resp.status_code = 200
         return resp
 
-    return app
 
 
 # Create the App
-app = create_app()
-
-http_server = WSGIServer(('', 8080), app)
-http_server.serve_forever()
+# app = create_app()
+if __name__ == '__main__':
+    http_server = WSGIServer(('', 8080), app)
+    http_server.serve_forever()
